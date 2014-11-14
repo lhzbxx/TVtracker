889887e8098ebbf3ef8c1504d1bb1b72dda51500
鲁浩 <lhzbxx@sjtu.edu.cn>
2014-11-14 20:38:10 +0800

    土豆和ACfun暂时放弃。

--- TVtracker.py
+++ TVtracker.py
@@ -1,7 +1,8 @@
-#coding:utf-8
+# -*- coding=utf-8 -*-
 import urllib2
 import re
 from bs4 import BeautifulSoup
+import PyV8
 
 f = open('TVtracker.html', 'w')
 
@@ -15,16 +16,48 @@ def matchTV_tencent(url, title):
         print 'http://v.qq.com' + i['href']
 
 def matchTV_youku(url, title):
-    content = urllib2.urlopen(url)
-    pattern = re.compile(r'<div id="episode">([\s\S]*?)</div></div>')
-    html = content.read()
-    match = pattern.search(html)
-    html = match.group()
-    pattern = re.compile(r'<li>([\s\S]*?)</li>')
-    match = pattern.search(html)
-    f.write('<h3>'+title+'</h3><ul>')
-    f.write(match.group())
-    f.write('</ul>')
+    content = urllib2.urlopen(url).read()
+    bs = BeautifulSoup(content)
+    a_list = bs.find('div', id='episode').find_all('a')
+    j = 0
+    for i in a_list:
+        j = j + 1
+        print i['href']
 
-matchTV_youku('http://www.youku.com/show_page/id_z6ae8ab240d5d11e4b8b7.html', '妙警贼探')
-matchTV_tencent('http://v.qq.com/cover/8/8asm6qy0sj9gn4v/p0015qvsnvk.html', '再造淑女')
+def matchTV_iqiyi(url, title):
+    content = urllib2.urlopen(url).read()
+    bs = BeautifulSoup(content)
+    a_list = bs.find_all('a', rseat='jj-list-text-video-0923')
+    j = 0
+    for i in a_list:
+        j = j + 1
+        print i['href']
+
+def matchTV_tudou(url, title):
+    # 妈了个屯，土豆用的JS动态生成的网页，先放弃这个源。
+    content = urllib2.urlopen(url).read()
+    bs = BeautifulSoup(content)
+    a_list = bs.find('div', class_='series_panel current').find_all('a')
+    j = 0
+    for i in a_list:
+        j = j + 1
+        print i['href']
+
+def matchTV_acfun(url, title):
+    content = urllib2.urlopen('http://static.acfun.mm111.net/dotnet/20130418/??script/jquery.2.1.1.min.js,script/prepare.0.0.5.min.js').read() + urllib2.urlopen('http://static.acfun.mm111.net/dotnet/20130418/??script/core.0.3.14.min.js,script/ready.0.3.23.min.js').read() + urllib2.urlopen('http://static.acfun.mm111.net/dotnet/20130418/script/album/album.min.js?date=1415866626945').read()
+    ctxt = PyV8.JSContext()
+    ctxt.enter()
+    func = ctxt.eval(content.decode('utf-8'))
+    print func()
+    # bs = BeautifulSoup(content)
+    # a_list = bs.find('div', id='block-list-sp').find_all('a')
+    # j = 0
+    # for i in a_list:
+    #     j = j + 1
+    #     print i['href']
+
+# matchTV_youku('http://www.youku.com/show_page/id_z6ae8ab240d5d11e4b8b7.html', '妙警贼探第六季')
+# matchTV_tencent('http://v.qq.com/cover/8/8asm6qy0sj9gn4v/p0015qvsnvk.html', '再造淑女第一季')
+# matchTV_iqiyi('http://www.iqiyi.com/a_19rrifrm2n.html', '爱情公寓4')
+# matchTV_tudou('http://www.tudou.com/albumcover/SgcffZHoj-I.html', '神盾局特工第二季')
+# matchTV_acfun('http://www.acfun.tv/a/aa1464868', u'生活大爆炸第八季')
